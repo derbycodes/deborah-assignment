0c8a60b71d47ad8f3c473273ea8a8b7d72847b82
derbycodes <137207490+derbycodes@users.noreply.github.com>
2024-11-20 07:09:48 +0000

    SimpleCalculator.py

---
+++ SimpleCalculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+
+if operator == '+':
+    result = num1 + num2
+
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    result = num1 / num2 
